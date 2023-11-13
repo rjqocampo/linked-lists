4f139e6d3f7d2e679e4b3b22f6b56746a54f3385
RJ Ocampo <rjqocampo@gmail.com>
2023-11-14 01:29:51 +0800

    Add prepend feature

--- script.js
+++ script.js
@@ -9,20 +9,16 @@ function createLinkedList() {
     return head;
   }
 
-  function nextNode() {
-    return head.nextNode;
-  }
-
   function append(value, node) {
     if (head === null) {
       head = createNode(value);
       return;
     }
 
-    const current = head || node;
+    const current = node || head;
 
-    while (current.nextNode !== null) {
-      append(value, current);
+    if (current.nextNode !== null) {
+      append(value, current.nextNode);
     }
 
     if (current.nextNode === null) {
@@ -30,12 +26,32 @@ function createLinkedList() {
     }
   }
 
-  return { firstNode, nextNode, append };
+  function prepend(value) {
+    const temporary = head;
+    head = createNode(value);
+    head.nextNode = temporary;
+  }
+
+  return { firstNode, append, prepend };
 }
 
 const linkedList = createLinkedList();
+// console.log(linkedList.firstNode());
+
+linkedList.append("A");
+// console.log(linkedList.firstNode());
+
+linkedList.append("B");
+// console.log(linkedList.firstNode());
+
+linkedList.append("C");
+// console.log(linkedList.firstNode());
+
+linkedList.prepend("X");
 console.log(linkedList.firstNode());
-linkedList.append(1);
+
+linkedList.prepend("Y");
 console.log(linkedList.firstNode());
-linkedList.append(2);
+
+linkedList.prepend("Z");
 console.log(linkedList.firstNode());
