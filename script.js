ae81c0f7ff75830bb5773c9de12623c5775174c4
RJ Ocampo <rjqocampo@gmail.com>
2023-11-14 00:54:10 +0800

    Add append feature

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-function createLinkedList() {}
-
-function createNode() {
-  const value = null;
-  const nextNode = null;
-
+function createNode(value = null, nextNode = null) {
   return { value, nextNode };
 }
+
+function createLinkedList() {
+  let head = null;
+
+  function firstNode() {
+    return head;
+  }
+
+  function nextNode() {
+    return head.nextNode;
+  }
+
+  function append(value, node) {
+    if (head === null) {
+      head = createNode(value);
+      return;
+    }
+
+    const current = head || node;
+
+    while (current.nextNode !== null) {
+      append(value, current);
+    }
+
+    if (current.nextNode === null) {
+      current.nextNode = createNode(value);
+    }
+  }
+
+  return { firstNode, nextNode, append };
+}
+
+const linkedList = createLinkedList();
+console.log(linkedList.firstNode());
+linkedList.append(1);
+console.log(linkedList.firstNode());
+linkedList.append(2);
+console.log(linkedList.firstNode());
